52cc4f0c6806a5db5756eac7972fcf2da6a75c9e
Ed Zillion <edzillion@gmail.com>
2017-08-03 00:48:17 +0200

    firebase init issue

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -17,7 +17,6 @@ import { User } from '../../interfaces/user-interface';
 import { SearchPage } from '../search/search';
 import { UserDetailPage } from '../user-detail/user-detail';
 
-
 @Component({
   selector: 'page-profile',
   templateUrl: 'profile.html',
@@ -32,6 +31,8 @@ export class ProfilePage {
   private providers: Array<any>;
   private user: User = {} as User;
 
+  private fileSelected: any;
+
   constructor(
     private navCtrl: NavController,
     private camera: Camera,
@@ -39,7 +40,8 @@ export class ProfilePage {
     private ds: DomSanitizer,
     private toastCtrl: ToastController,
     private userService: UserService,
-    private validatorService: ValidatorService
+    private validatorService: ValidatorService,
+    private sanitizer:DomSanitizer
   ) { }
 
   ionViewDidLoad() {
@@ -56,51 +58,91 @@ export class ProfilePage {
     );
   }
 
-  private selectFromGallery(): void {
-    var options = {
-      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
-      destinationType: this.camera.DestinationType.DATA_URL
-    };
-    this.camera.getPicture(options).then(
-      imageData => {
-        // imageData is a base64 encoded string
-        this.base64ImageData = imageData;
-        this.profilePicURL = "data:image/jpeg;base64," + imageData;
+  private isUploadSupported() {
+    if (navigator.userAgent.match(/(Android (1.0|1.1|1.5|1.6|2.0|2.1))|(Windows Phone (OS 7|8.0))|(XBLWP)|(ZuneWP)|(w(eb)?OSBrowser)|(webOS)|(Kindle\/(1.0|2.0|2.5|3.0))/)) {
+        return false;
+    }
+    var elem = document.createElement('input');
+    elem.type = 'file';
+    return !elem.disabled;
+  }
+
+  public fileChangeEvent(fileInput: any){
+      if (fileInput.target.files && fileInput.target.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = (e) => {
+            this.base64ImageData = e.target['result'];
+            this.profilePicURL = this.base64ImageData;
+        }
+
+        reader.readAsDataURL(fileInput.target.files[0]);
+    }
+}
+
+  private fileUpload() {
+    debugger;
+    let uploadTask = firebase.storage().ref('/profilepics').child(this.user.uid).putString(this.base64ImageData, 'base64', { contentType: 'image/jpg' });
+
+    uploadTask.on(
+      firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+      function(snapshot) {
+        debugger;
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        // var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        // console.log('Upload is ' + progress + '% done');
+        // switch (snapshot.state) {
+        //   case firebase.storage.TaskState.PAUSED: // or 'paused'
+        //     console.log('Upload is paused');
+        //     break;
+        //   case firebase.storage.TaskState.RUNNING: // or 'running'
+        //     console.log('Upload is running');
+        //     break;
+        // }
       },
-      error => {
+      function(error) {
         this.toast = this.toastCtrl.create({
-          message: 'Error selecting from gallery: ' + error,
+          message: 'Error uploading image: ' + error,
           duration: 3000,
           position: 'middle'
         });
         console.error(error);
         this.toast.present();
       });
+      //,
+      // function() {
+      //   // Upload completed successfully, now we can get the download URL
+      //   console.log('Upload Complete');
+      // });
+
+    // uploadTask.then((obj) => {
+    //   this.user.profilePicURL = uploadTask.snapshot.downloadURL;
+    //
+    //   this.db.object(`users/${this.UID}`).set(this.user)
+    //     .then((success) => {
+    //
+    //       console.log('userData save success');
+    //       this.pushService.initialise().then( () => {
+    //         console.log('pushService initialised');
+    //         this.formState.loading.dismiss();
+    //         if (this.formState.refilling)
+    //           this.navCtrl.setRoot(TabsPage);
+    //       });
+    //     })
+    //     .catch(
+    //     error => {
+    //       this.toast = this.toastCtrl.create({
+    //         message: 'Error saving user: ' + error,
+    //         duration: 3000,
+    //         position: 'middle'
+    //       });
+    //       console.error(error);
+    //       this.toast.present();
+    //     });
+    // });
   }
 
-  private openCamera(): void {
-    var options = {
-      sourceType: this.camera.PictureSourceType.CAMERA,
-      destinationType: this.camera.DestinationType.DATA_URL
-    };
-    this.camera.getPicture(options).then(
-      imageData => {
-        // imageData is a base64 encoded string
-        this.base64ImageData = imageData;
-        this.profilePicURL = "data:image/jpeg;base64," + imageData;
-      },
-      error => {
-        this.toast = this.toastCtrl.create({
-          message: 'Error opening camera: ' + error,
-          duration: 3000,
-          position: 'middle'
-        });
-        console.error(error);
-        this.toast.present();
-      });
-  }
-
-  saveProfile() {
+  private saveProfile() {
     this.user.displayName = this.user.firstName + ' ' + this.user.lastName;
     this.userService.saveUser();
     this.navCtrl.pop();
